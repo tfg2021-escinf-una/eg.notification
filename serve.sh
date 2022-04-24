df4b756619ecaa4126e4e1486f5e01361158e8be
Lobo, Edwin <edlobo98@gmail.com>
2022-04-24 17:33:57 -0600

    fixing serve.sh

--- serve.sh
+++ serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd app
-export email="YOUR_EMAIL_HERE"
-export password="YOUR_PASSWORD_HERE"
+export EmailAddress="YOUR EMAIL HERE"
+export EmailPassword="YOUR PASS HERE"
 export FLASK_APP="app"
 flask run

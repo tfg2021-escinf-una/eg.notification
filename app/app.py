4d2746f18ac24a3a7cc4c6728ae8ffd523ef74b9
Lobo, Edwin <edlobo98@gmail.com>
2022-04-24 15:59:07 -0600

    updating env var names

--- app/app.py
+++ app/app.py
@@ -6,8 +6,8 @@ mail = Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = os.environ['email']
-app.config['MAIL_PASSWORD'] = os.environ['password']
+app.config['MAIL_USERNAME'] = os.environ['EmailAddress']
+app.config['MAIL_PASSWORD'] = os.environ['EmailPassword']
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
